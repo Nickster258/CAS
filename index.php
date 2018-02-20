115c0c6cf73beb89d739dc933cbca2b26ca06821
Nickster258 <nickster258@gmail.com>
2018-02-20 12:36:04 -0500

    Cleaned index

--- index.php
+++ index.php
@@ -32,9 +32,13 @@
 <div class="outer">
 	<div class="middle">
 		<div class="inner">
-			<?php
-			include 'indexHelper.php';
-			?>
+			<p>Login</br></br>
+				<form action=\"register.php\" method=\"post\">
+					Name: <input type=\"text\" name=\"name\"><br>
+					Password: <input type=\"password\" name=\"pass\"><br>
+					<input type=\"submit\">
+				</form>
+			</p>
 		</div>
 	</div>
 </div>
