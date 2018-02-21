d0d5421018034463bb7199f0517bc3c8ec6c73d3
Nickster258 <nickster258@gmail.com>
2018-02-20 20:50:22 -0500

    Added some insane preg_match

--- register.php
+++ register.php
@@ -13,15 +13,15 @@ if(!$conn) {
 	echo "hi";
 } else {
 	global $conn;
-	$query = "CREATE TABLE IF NOT EXISTS users(uid VARCHAR(16), m_uuid VARCHAR(32), username VARCHAR(32), password VARCHAR(60), salt VARCHAR(\"$salt_length\"), email VARCHAR(60), verified BOOLEAN)";
-	$result = mysql_query($query, $conn);
 }
 
 function register_user($m_uuid, $name, $hash, $salt, $email) {
 	global $conn;
-	$emailToken = random(16, "rand");
+	$email_token = random(16, "rand");
 	$uid = random(16, "uid");
-	$query = "INSERT INTO users(uid, mUuid, username, password, salt, email, emailToken, verified) VALUES(\"$uid\", \"$mUuid\", \"$username\", \"$hash\", \"$salt\", \"$email\", \"$email_token\", false)";
+	$escaped_name = mysql_real_escape_string($name);
+	$escaped_email = mysql_real_escape_string($email);
+	$query = "INSERT INTO users(uid, m_uuid, username, password, salt, email, emailToken, verified) VALUES(\"$uid\", \"$m_uuid\", \"$escaped_name\", \"$hash\", \"$salt\", \"$escaped_email\", \"$email_token\", false)";
 	$result = mysql_query($query, $conn);
 	$query_token = "INSERT INTO email_tokens(uid, email, emailToken) VALUES (\"$uid\", \"$email\", \"$email_token\")";
 	if ($result) {
@@ -49,15 +49,31 @@ function get_hash($password, $salt) {
 }
 
 function get_muuid($token) {
+
 }
 
 function is_valid_name($name) {
+	if(preg_match('~^(?:[\p{L}\p{Mn}\p{Pd}\'\x{2019}]+\s[\p{L}\p{Mn}\p{Pd}\'\x{2019}]+\s?)+$~u', $name)) {
+		return $name;
+	} else {
+		return false;
+	}
 }
 
 function is_valid_email($email) {
+	if(preg_match("/^(?!(?:(?:\x22?\x5C[\x00-\x7E]\x22?)|(?:\x22?[^\x5C\x22]\x22?)){255,})(?!(?:(?:\x22?\x5C[\x00-\x7E]\x22?)|(?:\x22?[^\x5C\x22]\x22?)){65,}@)(?:(?:[\x21\x23-\x27\x2A\x2B\x2D\x2F-\x39\x3D\x3F\x5E-\x7E]+)|(?:\x22(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21\x23-\x5B\x5D-\x7F]|(?:\x5C[\x00-\x7F]))*\x22))(?:\.(?:(?:[\x21\x23-\x27\x2A\x2B\x2D\x2F-\x39\x3D\x3F\x5E-\x7E]+)|(?:\x22(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21\x23-\x5B\x5D-\x7F]|(?:\x5C[\x00-\x7F]))*\x22)))*@(?:(?:(?!.*[^.]{64,})(?:(?:(?:xn--)?[a-z0-9]+(?:-[a-z0-9]+)*\.){1,126}){1,}(?:(?:[a-z][a-z0-9]*)|(?:(?:xn--)[a-z0-9]+))(?:-[a-z0-9]+)*)|(?:\[(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){7})|(?:(?!(?:.*[a-f0-9][:\]]){7,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?)))|(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){5}:)|(?:(?!(?:.*[a-f0-9]:){5,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3}:)?)))?(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))(?:\.(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))){3}))\]))$/iD", $email)) {
+		return $email;
+	} else {
+		return false;
+	}
 }
 
 function is_valid_pass($pass) {
+	if(preg_match("/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,16}$/", $pass)) {
+		return $pass;
+	} else {
+		return false;
+	}
 }
 
 if (isset($_SERVER["REQUEST_METHOD"])) {
@@ -86,7 +102,7 @@ if (isset($_SERVER["REQUEST_METHOD"])) {
 	} else if (isset($_GET["token"])) {
 		$token = $_GET["token"];
 		$_SESSION["token"] = $token;
-		if (preg_math('/[a-z]/i', $token) && is_valid_token($token)) {
+		if (preg_match('/[a-z]/i', $token) && is_valid_token($token)) {
 			$mUuid = get_muuid($token);
 			$_SESSION["m_uuid"] = $mUuid;
 			$location = "Location: " . $URL . "register.php";
