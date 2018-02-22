0c6ad2d619e36176a5e023a425f9a544f8431530
Nickster258 <nickster258@gmail.com>
2018-02-22 16:27:20 -0500

    Removed quoted remnants that should have been removed in previous push

--- setup.php
+++ setup.php
@@ -7,13 +7,4 @@ global $pdo;
 $handler = new DatabaseHandler($pdo);
 
 print_r($handler->setup());
-/*
-try {
-	$query = $handle->query("CREATE TABLE IF NOT EXISTS auth_users(uid VARCHAR(16), m_uuid VARCHAR(32), username VARCHAR(32), password VARCHAR(60), email VARCHAR(64), verified BOOLEAN, UNIQUE KEY(uid))");
-	$query = $handle->query("CREATE TABLE IF NOT EXISTS auth_emailtokens(uid VARCHAR(16), email VARCHAR(64), email_token VARCHAR(16), UNIQUE KEY(uid))");
-	$query = $handle->query("CREATE TABLE IF NOT EXISTS auth_tokens(token VARCHAR(16), m_uuid VARCHAR(32), time INT, UNIQUE KEY(m_uuid))");
-} catch (PDOException $e) {
-	echo  '<pre>' . $e->getMessage() . '</pre>';
-	die();
-}*/
 ?>
